a179d663cd7e8a36779a41df9eb68e5e02703927
david <davidmcconkey@outlook.com>
2023-06-28 16:56:21 -0400

    implement changes

---
+++ sorted-frequency.js
@@ -0,0 +1,9 @@
+function sortedFrequency(arr, val) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) count++;
+  }
+  return count;
+}
+
+module.exports = sortedFrequency;
